5298fe78cac1543e0372ca771b86d655f9cfbcb3
SrividyaKK <SrividyaKK@users.noreply.github.com>
2021-04-04 12:27:28 +0530

    Select file

--- client/src/components/File.tsx
+++ client/src/components/File.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { IItem } from '../pages/Drive/ListFolderItems';
+import { setSelectedItem } from '../redux/drive/drive.actions';
 import Card from './common/Card';
 
 interface IProps {
@@ -7,8 +9,17 @@ interface IProps {
 }
 
 const File = ({ file }: IProps) => {
+  const dispatch = useDispatch();
+  const handleFileClick = () => {
+    // console.log('Clicked: ', file._id.$oid);
+    dispatch(setSelectedItem({ type: file.type, id: file._id.$oid }));
+  };
   return (
-    <Card className='ow w4 pa3' style={{ height: '150px' }}>
+    <Card
+      className='ow w4 pa3 pointer'
+      style={{ height: '150px' }}
+      onClick={handleFileClick}
+    >
       {file.name}
     </Card>
   );
