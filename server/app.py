6b84a3c2fea125cdd7231f51fd3b258e7095cc6a
Shivam Kumar <shikuma@microsoft.com>
2021-04-09 03:54:20 +0530

    Added rename and delete

--- server/app.py
+++ server/app.py
@@ -9,13 +9,14 @@ from flask import jsonify
 from passlib.hash import pbkdf2_sha256
 from werkzeug.utils import secure_filename
 import os, uuid
-from azure.storage.blob import BlobServiceClient,generate_blob_sas, BlobSasPermissions, BlobClient, ContainerClient, __version__
+from azure.storage.blob import BlobServiceClient,generate_blob_sas, BlobSasPermissions,ContentSettings , BlobClient, ContainerClient, __version__
 import datetime
 from datetime import datetime, timedelta
 from flask_cors import CORS
 import uuid
 import boto3
 import os
+import mimetypes
 
 app = Flask(__name__)
 
@@ -51,7 +52,7 @@ s3_resource = aws_session.resource('s3')
 s3_client = aws_session.client('s3')
 
 # Create the BlobServiceClient object which will be used to create a container client
-blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
+blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string, headers={'Content-Disposition': 'inline'})
 
 @app.route("/")
 @app.route("/main")
@@ -137,24 +138,12 @@ def addFolder():
         
         folder_id = request.headers['username'] + '_folder_' + str(uuid.uuid4())
 
-<<<<<<< HEAD
-        userCollection.insert_one({'name' : secure_filename(request.form['folderName']),
-        'created' : datetime.utcnow(),
-        'accessed' : datetime.utcnow(),
-        'modified' : datetime.utcnow(),
-        'folder_id': folder_id,
-        'type' : 'folder',
-        'parentDir': currentDir,
-        'absolutePath': absolutePath
-        })
-
-        resp = jsonify("Successfully added folder")
-=======
         payload = {
             'name' : secure_filename(request.form['folderName']),
-            'created' : datetime.datetime.utcnow(),
-            'accessed' : datetime.datetime.utcnow(),
-            'modified' : datetime.datetime.utcnow(),
+            'created' : datetime.utcnow(),
+            'folder_id': folder_id,
+            'accessed' : datetime.utcnow(),
+            'modified' : datetime.utcnow(),
             'type' : 'folder',
             'parentDir': currentDir,
             'absolutePath': absolutePath
@@ -162,8 +151,7 @@ def addFolder():
         x = userCollection.insert_one(payload)
 
         # resp = jsonify("Successfully added folder")
-        resp = jsonify({**payload, '_id': x.inserted_id})
->>>>>>> 6909b80998a93a323f9248d4afc2a33e75be1e68
+        resp = jsonify({**payload, '_id': folder_id})
         resp.status_code = 200
         resp.headers.add("Access-Control-Allow-Origin", "*")
         return resp
@@ -189,6 +177,8 @@ def upload():
         users = mongo.users.info
         userData = users.find_one({'username': request.headers['username']})
 
+        file_mime_type, _ = mimetypes.guess_type(file_id)
+
         if 'cloudProvider' in request.form:
             cloudProvider = request.form['cloudProvider']
         else:
@@ -196,15 +186,19 @@ def upload():
 
         if cloudProvider == 'Azure':
             #uploading to azure storage
+            my_content_settings = ContentSettings(content_type=file_mime_type, content_disposition= 'inline')
+
             blob_client = blob_service_client.get_blob_client(container=userData['container'], blob=file_id)
             with open(f_path, "rb") as data:
-                blob_client.upload_blob(data)
+                blob_client.upload_blob(data, content_settings=my_content_settings)
 
         else:
             #uploading to aws s3
             print('EnteringAWS')
             print(userData['container'])
-            s3_resource.Bucket(aws_bucket_name).upload_file(f_path, userData['container']+'/'+file_id)
+            print(file_mime_type)
+            #s3_resource.Bucket(aws_bucket_name).upload_file(f_path, userData['container']+'/'+file_id)
+            s3_resource.Bucket(aws_bucket_name).upload_file(f_path, userData['container']+'/'+file_id, ExtraArgs={'ContentType': file_mime_type, 'ContentDisposition' : 'inline' })
         
         #TODO: Add exception handling for cases such as blob already exsiting 
         
@@ -221,25 +215,13 @@ def upload():
         else:
             absolutePath = '/root'
 
-<<<<<<< HEAD
-        userCollection.insert_one({'name' : secure_filename(f.filename),
-        'file_id' : file_id,
-        'created' : datetime.utcnow(),
-        'accessed' : datetime.utcnow(),
-        'modified' : datetime.utcnow(),
-        'type' : 'file',
-        'parentDir': currentDir,
-        'absolutePath': absolutePath,
-        'cloudProvider': cloudProvider
-        })
 
-        #delete file after upload from server
-=======
         payload = {
             'name' : secure_filename(f.filename),
-            'created' : datetime.datetime.utcnow(),
-            'accessed' : datetime.datetime.utcnow(),
-            'modified' : datetime.datetime.utcnow(),
+            'file_id' : file_id,
+            'created' : datetime.utcnow(),
+            'accessed' : datetime.utcnow(),
+            'modified' : datetime.utcnow(),
             'type' : 'file',
             'parentDir': currentDir,
             'absolutePath': absolutePath,
@@ -249,11 +231,10 @@ def upload():
         x = userCollection.insert_one(payload)
 
         # delete file after upload from server
->>>>>>> 6909b80998a93a323f9248d4afc2a33e75be1e68
         os.remove(f_path)
 
         # resp = jsonify("Successfully uploaded file")
-        resp = jsonify({ **payload, '_id': x.inserted_id })
+        resp = jsonify({ **payload, '_id': file_id })
         resp.status_code = 200
         resp.headers.add("Access-Control-Allow-Origin", "*")
         return resp
@@ -292,6 +273,56 @@ def getFile():
 
         #TODO: check if blob is a part of users blobs
         #blob = BlobClient.from_connection_string(conn_str=azure_connection_string,container_name=session['username'] , blob_name=request.form['fileName'])
+        print('awsdddddddddddd')
+        fileData = userCollection.find_one({'file_id': request.headers['file_id']})
+        fileId = request.headers['file_id']
+        cloudProvider = fileData['cloudProvider']
+        fileName = fileData['name']
+
+        users = mongo.users.info
+        userData = users.find_one({'username': request.headers['username']})
+        
+        if cloudProvider == 'Azure':
+            #TODO: try implementing a block blob service
+            print(userData['container'])
+            print('a1')
+            sas_blob = generate_blob_sas(account_name='vicarastorage', 
+                                    container_name=userData['container'],
+                                    blob_name=fileId,
+                                    account_key='pDdAKH74ZV4HbXaWKcYXNS1OXR6rJ3fvSD7T//dQIbYyfLvhS0BZdVNy0sllTFyl1zsdvf0b7v6OgP5Y6NuOCg==',
+                                    permission=BlobSasPermissions(read=True),
+                                    expiry=datetime.utcnow() + timedelta(minutes=30))
+            #blob.generate_blob_shared_access_signature(container_name=session['username'],blob_name=request.form['fileName'], permission = READ,expiry = datetime.utcnow() + timedelta(minutes=30))
+            #block_blob_service = BlockBlobService(conn_str= azure_connection_string)
+            url = 'https://'+'vicarastorage'+'.blob.core.windows.net/'+userData['container']+'/'+fileId+'?'+sas_blob
+            print(url)
+            resp = jsonify(url)
+            resp.status_code = 200
+            resp.headers.add("Access-Control-Allow-Origin", "*")
+            return resp
+        else:
+            file_full_id = userData['container'] + '/' + fileId
+            url = s3_client.generate_presigned_url(ClientMethod='get_object',
+                                     Params={'Bucket': aws_bucket_name, 'Key': file_full_id}, ExpiresIn=3600)
+            resp = jsonify(url)
+            resp.status_code = 200
+            resp.headers.add("Access-Control-Allow-Origin", "*")
+            return resp
+
+    resp = jsonify('Invalid access request')
+    resp.status_code = 403
+    resp.headers.add("Access-Control-Allow-Origin", "*")
+    return resp
+
+@app.route('/getFileLink', methods = ['GET'])
+def getFileLink():
+    #TODO create way of fetching file
+    if 'username' in request.headers:
+        #TODO: Implement a different name for blobs that user given name to handle same name for files at different paths, or directly give abs path name
+        userCollection = mongo.drive[request.headers['username']]
+
+        #TODO: check if blob is a part of users blobs
+        #blob = BlobClient.from_connection_string(conn_str=azure_connection_string,container_name=session['username'] , blob_name=request.form['fileName'])
 
         fileData = userCollection.find_one({'file_id': request.headers['file_id']})
         fileId = request.headers['file_id']
@@ -303,58 +334,142 @@ def getFile():
         
         if cloudProvider == 'Azure':
             #TODO: try implementing a block blob service
+            print(userData['container'])
+            print("this is 2")
             sas_blob = generate_blob_sas(account_name='vicarastorage', 
-                                    container_name=request.headers['username'],
+                                    container_name=userData['container'],
                                     blob_name=fileId,
                                     account_key='pDdAKH74ZV4HbXaWKcYXNS1OXR6rJ3fvSD7T//dQIbYyfLvhS0BZdVNy0sllTFyl1zsdvf0b7v6OgP5Y6NuOCg==',
                                     permission=BlobSasPermissions(read=True),
                                     expiry=datetime.utcnow() + timedelta(minutes=30))
             #blob.generate_blob_shared_access_signature(container_name=session['username'],blob_name=request.form['fileName'], permission = READ,expiry = datetime.utcnow() + timedelta(minutes=30))
             #block_blob_service = BlockBlobService(conn_str= azure_connection_string)
-            url = 'https://'+'vicarastorage'+'.blob.core.windows.net/'+request.headers['username']+'/'+request.form['fileName']+'?'+sas_blob
+            url = 'https://'+'vicarastorage'+'.blob.core.windows.net/'+userData['container']+'/'+fileId+'?'+sas_blob
             print(url)
             resp = jsonify(url)
             resp.status_code = 200
             resp.headers.add("Access-Control-Allow-Origin", "*")
             return resp
         else:
-            #print('Somehththth')
             file_full_id = userData['container'] + '/' + fileId
-            print(file_full_id)
-            s3_resource.Bucket(aws_bucket_name).download_file(file_full_id, fileName)
-            resp = jsonify("aws file downloaded")
+            file_mime_type, _ = mimetypes.guess_type(fileId)
+            url = s3_client.generate_presigned_url(ClientMethod='get_object',
+                                     Params={'Bucket': aws_bucket_name, 'Key': file_full_id, 'ResponseContentType': file_mime_type}, ExpiresIn=3600)
+            resp = jsonify(url)
             resp.status_code = 200
             resp.headers.add("Access-Control-Allow-Origin", "*")
             return resp
+
     resp = jsonify('Invalid access request')
     resp.status_code = 403
     resp.headers.add("Access-Control-Allow-Origin", "*")
     return resp
 
-@app.route('/getFileLink', methods = ['GET'])
-def getFileLink():
+@app.route('/rename', methods = ['PUT'])
+def renameFile():
+    #if user is logged in
     if 'username' in request.headers:
-        #TODO: Implement a different name for blobs that user given name to handle same name for files at different paths, or directly give abs path name
+        #adding file metadata to mongoDB as part of users collection
         userCollection = mongo.drive[request.headers['username']]
+        userCollection.update_one({'file_id': request.form['fileID']},{ "$set": { 'name': request.form['newName'] } })
 
-        #TODO: check if blob is a part of users blobs
-        #blob = BlobClient.from_connection_string(conn_str=azure_connection_string,container_name=session['username'] , blob_name=request.form['fileName'])
-
-        #TODO: try implementing a block blob service
-        sas_blob = generate_blob_sas(account_name='vicarastorage', 
-                                container_name=request.headers['username'],
-                                blob_name=request.form['fileName'],
-                                account_key='pDdAKH74ZV4HbXaWKcYXNS1OXR6rJ3fvSD7T//dQIbYyfLvhS0BZdVNy0sllTFyl1zsdvf0b7v6OgP5Y6NuOCg==',
-                                permission=BlobSasPermissions(read=True),
-                                expiry=datetime.utcnow() + timedelta(minutes=1))
-        #blob.generate_blob_shared_access_signature(container_name=session['username'],blob_name=request.form['fileName'], permission = READ,expiry = datetime.utcnow() + timedelta(minutes=30))
-        #block_blob_service = BlockBlobService(conn_str= azure_connection_string)
-        url = 'https://'+'vicarastorage'+'.blob.core.windows.net/'+request.headers['username']+'/'+request.form['fileName']+'?'+sas_blob
-        print(url)
-        resp = jsonify(url)
+        resp = jsonify('file renamed successfully')
         resp.status_code = 200
         resp.headers.add("Access-Control-Allow-Origin", "*")
         return resp
+
+    resp = jsonify('Login to be able to rename a file')
+    resp.status_code = 403
+    resp.headers.add("Access-Control-Allow-Origin", "*")
+    return resp
+
+
+@app.route('/deleteFile', methods = ['DELETE'])
+def deleteFile():
+    #if user is logged in
+    if 'username' in request.headers: 
+        #adding file metadata to mongoDB as part of users collection
+        userCollection = mongo.drive[request.headers['username']]
+        userCollection.delete_one({'file_id': request.form['fileID']})
+        resp = jsonify('file deleted successfully')
+        resp.status_code = 200
+        resp.headers.add("Access-Control-Allow-Origin", "*")
+        return resp
+        
+        #TODO: Perform actual delete
+        # print(file_id)
+
+        # users = mongo.users.info
+        # userData = users.find_one({'username': request.headers['username']})
+
+        # file_mime_type, _ = mimetypes.guess_type(file_id)
+
+        # if 'cloudProvider' in request.form:
+        #     cloudProvider = request.form['cloudProvider']
+        # else:
+        #     cloudProvider = 'Azure'
+
+        # if cloudProvider == 'Azure':
+        #     #uploading to azure storage
+        #     my_content_settings = ContentSettings(content_type=file_mime_type, content_disposition= 'inline')
+
+        #     blob_client = blob_service_client.get_blob_client(container=userData['container'], blob=file_id)
+        #     with open(f_path, "rb") as data:
+        #         blob_client.upload_blob(data, content_settings=my_content_settings)
+
+        # else:
+        #     #uploading to aws s3
+        #     print('EnteringAWS')
+        #     print(userData['container'])
+        #     print(file_mime_type)
+        #     #s3_resource.Bucket(aws_bucket_name).upload_file(f_path, userData['container']+'/'+file_id)
+        #     s3_resource.Bucket(aws_bucket_name).upload_file(f_path, userData['container']+'/'+file_id, ExtraArgs={'ContentType': file_mime_type, 'ContentDisposition' : 'inline' })
+        
+        # #TODO: Add exception handling for cases such as blob already exsiting 
+        
+        # #adding file metadata to mongoDB as part of users collection
+        # userCollection = mongo.drive[request.headers['username']]
+
+        # if 'currentDir' in request.form:
+        #     currentDir = request.form['currentDir']
+        # else:
+        #     currentDir = '/root'
+
+        # if 'absolutePath' in request.form:
+        #     absolutePath = request.form['absolutePath']
+        # else:
+        #     absolutePath = '/root'
+
+
+        # payload = {
+        #     'name' : secure_filename(f.filename),
+        #     'file_id' : file_id,
+        #     'created' : datetime.utcnow(),
+        #     'accessed' : datetime.utcnow(),
+        #     'modified' : datetime.utcnow(),
+        #     'type' : 'file',
+        #     'parentDir': currentDir,
+        #     'absolutePath': absolutePath,
+        #     'cloudProvider': cloudProvider
+        # }
+
+        # x = userCollection.insert_one(payload)
+
+        # # delete file after upload from server
+        # os.remove(f_path)
+
+        # # resp = jsonify("Successfully uploaded file")
+        # resp = jsonify({ **payload, '_id': file_id })
+        # resp.status_code = 200
+        # resp.headers.add("Access-Control-Allow-Origin", "*")
+        # return resp
+
+    resp = jsonify('Login to be able to delete a file')
+    resp.status_code = 403
+    resp.headers.add("Access-Control-Allow-Origin", "*")
+    return resp
+
+    
 
 @app.route('/logout', methods = ['GET'])
 def logout():
